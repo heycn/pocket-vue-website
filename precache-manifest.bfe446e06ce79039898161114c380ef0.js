ee25f99006fe5505c6d47518246c1b4c848168af
heycn <x1chennan@qq.com>
2021-12-19 21:24:31 +0800

    deploy

---
+++ precache-manifest.bfe446e06ce79039898161114c380ef0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d66d525c0621964d156e",
+    "url": "/pocket-vue-website/css/app.5c9c8880.css"
+  },
+  {
+    "revision": "ed05bb255dbb4d8e80362415cde028c4",
+    "url": "/pocket-vue-website/index.html"
+  },
+  {
+    "revision": "d66d525c0621964d156e",
+    "url": "/pocket-vue-website/js/app.e85f4a0b.js"
+  },
+  {
+    "revision": "952aeed47dc4ea6beaad",
+    "url": "/pocket-vue-website/js/chunk-vendors.208ff160.js"
+  },
+  {
+    "revision": "e378b52366bbb9c63e89c9a64f1ea0b1",
+    "url": "/pocket-vue-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pocket-vue-website/robots.txt"
+  }
+]);
